c630fafa60747f77ac6fd00644cec2160a269fae
Oguzhan Sali <salioguzhan.61@gmail.com>
2024-12-06 00:09:15 +0300

    added Person data

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const phoneBook = [
+    {
+        avatar:"https://i.pravatar.cc/150?img=3",
+        name:"Jhon Doe" ,
+        phoneNumbers:[
+            {name:"home", number:"123456789"},
+            {name:"work", number:"987654321"}
+        ],
+        addresses: [
+            {name:"home", address:"1234 Main St"},
+            {name:"work", address:"4321 Main St"}
+        ],
+        emails: [
+            {name:"personel", emails:"jhon@doe.com"},
+            {name:"work", address:"jhon@acme.com"}
+        ],
+    },
+    {
+        avatar:"https://i.pravatar.cc/150?img=5",
+        name:"Jane Doe" ,
+        phoneNumbers:[
+            {name:"home", number:"546789159"},
+            {name:"work", number:"951236974"}
+        ],
+        addresses: [
+            {name:"home", address:"456 Main St"},
+            {name:"work", address:"987 Main St"}
+        ],
+        emails: [
+            {name:"personel", emails:"jane@doe.com"},
+            {name:"work", address:"jane@acme.com"}
+        ],
+    },
+]
